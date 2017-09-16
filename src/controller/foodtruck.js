af11467dcda6dd803f642f3afab38fa967b52399
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-09-16 07:49:49 +0200

    esercizio - sezione 5 lezione 33

--- src/controller/foodtruck.js
+++ src/controller/foodtruck.js
@@ -121,5 +121,16 @@ export default ({ config, db }) => {
     });
   });
 
+  // get foodtrucks for a specific foodtype
+  // /v1/foodtruck/foodtype/:foodtype
+  api.get('/foodtype/:foodtype', (req, res) => {
+    FoodTruck.find({ "foodtype": req.params.foodtype }, (err, foodtrucks) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(foodtrucks);
+    });
+  });
+
   return api;
 }
